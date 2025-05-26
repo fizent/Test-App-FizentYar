6c18b48d9c645fe7842583ac3db7a3f2c141b48c
88863-Vector-Hogo <logi.farst.leno@gmail.com>
2025-05-26 20:01:17 +0430

    True Address 3

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,12 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import backArrow from "../assets/icons8-back-arrow-64.png"; // مسیر مناسب با توجه به ساختار پوشه‌ها
 
 export default function Back_Btn() {
   const navigate = useNavigate();
 
   return (
     <div className="back_btn" onClick={() => navigate(-1)} style={{ cursor: 'pointer' }}>
-        <img className="back_image" src={backArrow} alt="Back" />
+      <img className="back_image" src="/src/assets/icons8-back-arrow-64.png" alt="Back" />
     </div>
   );
 }
